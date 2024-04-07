db7ccd610ca5415b099a4e8e2bb8fac04fbb77ba
Eric George <Cronoburn@users.noreply.github.com>
2024-04-07 11:42:54 -0400

    route placement and separation

--- app/blueprints/user.py
+++ app/blueprints/user.py
@@ -10,3 +10,14 @@ bp = Blueprint("user", __name__)
 @bp.route("/user/<name>")
 def user(name):
     return render_template("user.tpl", name=name, landing="user" )
+@bp.route("/name", methods=['GET','POST'])
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Name submitted and read successfully.")
+    return render_template("name.tpl",
+                           name=name,
+                           form=form, landing="name")
