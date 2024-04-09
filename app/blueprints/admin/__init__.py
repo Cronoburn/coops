24fa392923a7d5967741dfd7c003182b8940add6
Eric George <cronoburn@gmail.com>
2024-04-09 18:37:44 -0400

    admin

---
+++ app/blueprints/admin/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask,Blueprint, render_template, flash
+from flask_mde import Mde
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+
+bp = Blueprint("admin", __name__, template_folder="templates", static_folder="static")
+
+@bp.route('/')
+def home():
+    pass
